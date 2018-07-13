7a13640ad63e08a236ee6df6e97a5b5ae1252181
sigsant <sigfridsantamera@hotmail.com>
2018-07-13 17:25:03 +0200

    Mejorar legibilidad
    Sangria del texto en las aplicaciones

--- todo-app/js/funcionBotones.js
+++ todo-app/js/funcionBotones.js
@@ -1,41 +1,40 @@
-function ponerTarea(id, id2){
+function ponerTarea(id, id2) {
     var tarea = document.getElementById(id).value;
     var mostrarTarea = document.getElementById(id2);
-    
-    if(tarea.length > 3){ 
+
+    if (tarea.length > 3) {
         var item = document.createElement("div");
         item.textContent = tarea;
         item.style.letterSpacing = "1px";
-        item.style.marginTop="15px";
+        item.style.marginTop = "15px";
         item.style.marginBottom = "5px";
         mostrarTarea.appendChild(item);
-        
+
         //eliminamos este elemento al hacer click
         item.onclick = eliminar;
         //Al pulsar enter se vacia el contenido
         clearInput('insertarTexto');
-    }else{       
-        alert("Escribe al menos 4 carácteres"); 
+    } else {
+        alert("Escribe al menos 4 carácteres");
         return false;
     }
 }
 
 //Elimina el objeto al hacer click
-function eliminar(){
+function eliminar() {
     document.getElementById("mostrarTareas").removeChild(this);
 }
 
 //Permite introducir datos con Enter y vacia el input
-function submitEnter(e){
-    if(e.keyCode == 13){
-        ponerTarea('insertarTexto','mostrarTareas');
+function submitEnter(e) {
+    if (e.keyCode == 13) {
+        ponerTarea('insertarTexto', 'mostrarTareas');
         clearInput('insertarTexto');
-    }
-    else if(e.keyCode == 27){ //Con Esc vacia el input
+    } else if (e.keyCode == 27) { //Con Esc vacia el input
         clearInput('insertarTexto');
     }
 }
 
-function clearInput(id){
-    document.getElementById(id).value="";
+function clearInput(id) {
+    document.getElementById(id).value = "";
 }
